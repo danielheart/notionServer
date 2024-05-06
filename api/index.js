1de7dbde443d4b495d0eef07fa5dbe7e0961a550
danielheart <danielheart@yeah.net>
2024-05-06 20:34:27 +0800

    store access token locally, and cancel online save word to notion

--- api/index.js
+++ api/index.js
@@ -27,21 +27,7 @@ const {
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'))
 })
-// test
-app.get('/test', async function (req, res) {
-   const botId = '7b9f027f-c3b9-46f6-adb2-2707239f68d1'
-   const accessToken = 'secret_L3z6tdl6waQTm7XpRxSzHgke3VXpclZMOlzgtZGLHhY'
-   const databaseId = '78c82987-c683-4f80-b42d-2c0cca841246'
-   const workspaceId = 'my-workspace-id'
 
-   await kv.set(botId, { accessToken, databaseId, workspaceId })
-   const data = await kv.get(botId)
-   if (data) {
-      const { accessToken, databaseId, workspaceId } = data
-      // 使用 accessToken 和 databaseId
-   }
-   res.send(data)
-})
 // get redirect link code to authroize notion
 app.get('/redirect', async function (req, res) {
    const result = await getOauth(req)
@@ -51,6 +37,27 @@ app.get('/redirect', async function (req, res) {
       //store data to vercel cv
       await kv.set(botId, { accessToken, databaseId, workspaceId })
 
+      const html = `
+      <html>
+        <head>
+          <title>Dictionary to Notion</title>
+        </head>
+        <body>
+          <h1>Congratulations</h1>
+          <p>Authorize Notion successfully!</p>
+          <script>
+               var extensionId = 'deaiipjieiccllhfmlnblcnjlegopdka'
+               chrome.runtime.sendMessage(
+                  extensionId,
+                  { accessToken:"${accessToken}",databaseId:"${databaseId}"},
+                  function (response) {
+                     console.log(response)
+                  },
+               )
+          </script>
+        </body>
+      </html>
+    `
       // set cookies
       const expireDate = new Date()
       expireDate.setFullYear(expireDate.getFullYear() + 10) // 设置 Cookie 的过期时间为十年后
@@ -58,7 +65,7 @@ app.get('/redirect', async function (req, res) {
       res.cookie('botId', botId, cookieOptions)
          .cookie('accessToken', accessToken, cookieOptions)
          .cookie('databaseId', databaseId, cookieOptions)
-         .send('Authorize Notion successfully')
+         .send(html)
       console.log('Cookie generated successfully')
    } else {
       res.send(result.message)
@@ -66,31 +73,31 @@ app.get('/redirect', async function (req, res) {
 })
 
 // save word to notion database
-app.post('/saveWord', async function (req, res) {
+app.get('/oauth/notion', async function (req, res) {
    // 处理回调逻辑
-   if (req.cookies && req.cookies.accessToken) {
-      console.log('use Cookie access token')
-      // 返回响应或执行其他操作
-      saveWord(req.body, req.cookies.accessToken, req.cookies.databaseId)
-         .then((state) => {
-            res.json(state)
-         })
-         .catch((error) => {
-            console.log(error)
-            res.json({ message: 'error' })
-         })
-   } else {
-      // redirect if no cookie find
-      console.log('redirect to get access')
-      const authParams = new URLSearchParams({
-         client_id: NOTION_CLIENT_ID,
-         redirect_uri: NOTION_REDIRECT_URI,
-         response_type: 'code',
-         owner: 'user',
-      })
-      const authUrl = `https://api.notion.com/v1/oauth/authorize?${authParams}`
-      res.redirect(authUrl)
-   }
+   // if (req.cookies && req.cookies.accessToken) {
+   //    console.log('use Cookie access token')
+   //    // 返回响应或执行其他操作
+   //    saveWord(req.body, req.cookies.accessToken, req.cookies.databaseId)
+   //       .then((state) => {
+   //          res.json(state)
+   //       })
+   //       .catch((error) => {
+   //          console.log(error)
+   //          res.json({ message: 'error' })
+   //       })
+   // } else {
+   // redirect if no cookie find
+   console.log('redirect to get access')
+   const authParams = new URLSearchParams({
+      client_id: NOTION_CLIENT_ID,
+      redirect_uri: NOTION_REDIRECT_URI,
+      response_type: 'code',
+      owner: 'user',
+   })
+   const authUrl = `https://api.notion.com/v1/oauth/authorize?${authParams}`
+   res.redirect(authUrl)
+   // }
 })
 
 // listen for requests :)
